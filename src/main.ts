a7683698ca93621fea64f6350ac4f0531ead5008
秦雨 <qinyu@zto.cn>
2021-05-27 21:18:04 +0800

    .gitignore.eslintrc.js.browserslistrc

--- src/main.ts
+++ src/main.ts
@@ -9,15 +9,15 @@ createApp(App).use(store).use(router).mount("#app");
 
 
 
-var global
-function f1() {
-    function f2() {
-        var v2 = 999
-        v1 = 111
-        console.log(v1)
-    }
-    var v1 = 666
-    return f2
-}
-const f = f1()
-f()
+// var global
+// function f1() {
+//     function f2() {
+//         var v2 = 999
+//         v1 = 111
+//         console.log(v1)
+//     }
+//     var v1 = 666
+//     return f2
+// }
+// const f = f1()
+// f()
